ef5c8a216a1a52e3821c93c1c4ce2dcbbbce22e0
suddenbuff <suddenbuff@gmail.com>
2025-12-05 19:12:26 +0300

    deploy to ghpages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/issuesManager_spa/',    // чтобы работал путь /currency_spa
+  server: {
+    open: '/issuesManager_spa/'   // чтобы npm run dev открыл именно этот URL
+  },
   plugins: [react()],
 })
